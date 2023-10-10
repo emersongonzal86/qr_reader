07f332f3ebd953e9e2b6f21a4299813e400f5324
emerson <emersongonzal86@gmail.com>
2023-10-09 20:52:47 -0500

    application design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/custom_navigationbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
 
 class HomePage extends StatelessWidget {
-   
   const HomePage({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomePage'),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})
+        ],
       ),
+      body: Center(
+        child: Text('HomePage'),
+      ),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
