cb799748317a2739db24ecb4cc9722b0cf9688cc
emerson <emersongonzal86@gmail.com>
2023-10-18 19:14:09 -0500

    SQFLite create database an table

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/direcciones_page.dart';
 import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/providers/db_provider.dart';
 import 'package:qr_reader/providers/ui_provider.dart';
 
 import 'package:qr_reader/widgets/custom_navigationbar.dart';
@@ -35,6 +36,9 @@ class _HomePageBody extends StatelessWidget {
   Widget build(BuildContext context) {
     //Obtener el selected menu opt
     final uiProvider = Provider.of<UiProvider>(context);
+
+    //TODO: Temporal leer la base de datos
+    DBProvider.db.database;
 
     //cambiar para mostrar la pagina respectiva
     final currentIndex = uiProvider.selectedMenuOpt;
--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -1,19 +1,40 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DBProvider{
-  static Database _database;
+class DBProvider {
+  static Database? _database;
   static final DBProvider db = DBProvider._();
   DBProvider._();
 
-  Future<Database> get database async{
-    if(_database != null) return _database;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
 
     _database = await initDB();
 
-    return _database;
+    return _database!;
   }
 
-  Future<Database>initDB() async{
+  Future<Database> initDB() async {
+    //Path de donde almacenaremos la base de datos 
+    // la ruta fisica en la maquina local es /home/emerson/.cache/Google/AndroidStudio2022.3/device-explorer/Note-10 API 30/data/user/0/com.example.qr_reader/app_flutter
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+    print(path);
 
+    //crear base de datos
+
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+        CREATE TABLE Scans(
+          id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          tipo TEXT,
+          valor TEXT
+        )
+        ''');
+    });
   }
 }
