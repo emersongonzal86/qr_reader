4dc13d368a8e009583d5a18caa8ed055cbab112b
emerson <emersongonzal86@gmail.com>
2023-10-21 17:17:01 -0500

    Read Scans from the database

--- lib/providers/scan_list_provider.dart
+++ lib/providers/scan_list_provider.dart
@@ -13,11 +13,33 @@ class ScanListProvider extends ChangeNotifier {
 
     nuevoScan.id = id;
 
-    if(this.tipoSeleccionado == nuevoScan.tipo){
-
-        this.scans.add(nuevoScan);
-        notifyListeners();
+    if (this.tipoSeleccionado == nuevoScan.tipo) {
+      this.scans.add(nuevoScan);
+      notifyListeners();
     }
-    
+  }
+
+  cargarScans() async {
+    final scans = await DBProvider.db.getTodosLosScans();
+    this.scans = [...?scans];
+    notifyListeners();
+  }
+
+  cargarScansPorTipo(String tipo) async {
+    final scans = await DBProvider.db.getScansPorTipo(tipo);
+    this.scans = [...?scans];
+    this.tipoSeleccionado = tipo;
+    notifyListeners();
+  }
+
+  borrarTodos() async {
+    await DBProvider.db.deleteAllScans();
+    this.scans = [];
+    notifyListeners();
+  }
+
+  borrarScanPorId(int id) async {
+    await DBProvider.db.deteleScan(id);
+    this.cargarScansPorTipo(this.tipoSeleccionado);
   }
 }
