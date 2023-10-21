fb01f0a456681e8bd998b83ae5b3af5ea4b9ef25
emerson <emersongonzal86@gmail.com>
2023-10-21 17:48:16 -0500

    show UI scans without database

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 class MapasPage extends StatelessWidget {
-   
   const MapasPage({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mapas Page'),
-    
-    );
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (_, i) => ListTile(
+              leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+              title: Text('http://cualquiercosa'),
+              subtitle: Text('ID:1'),
+              trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+              onTap: () => print('abrir algo...'),
+            ));
   }
 }
